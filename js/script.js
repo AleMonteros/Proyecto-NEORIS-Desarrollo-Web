9ad182f3cf131c18446ca997248c5c3577f9014b
Heroo17 <149008528+Heroo17@users.noreply.github.com>
2023-11-15 13:59:39 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
+let currentStep = 1;
+    const form = document.getElementById('wizardForm');
+    const steps = document.querySelectorAll('.step');
 
+    function showStep(step) {
+        steps.forEach((s, index) => {
+            if (index + 1 === step) {
+                s.style.display = 'block';
+            } else {
+                s.style.display = 'none';
+            }
+        });
+    }
+
+    function nextStep() {
+        if (currentStep < steps.length) {
+            currentStep++;
+            showStep(currentStep);
+        }
+    }
+
+    function prevStep() {
+        if (currentStep > 1) {
+            currentStep--;
+            showStep(currentStep);
+        }
+    }
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        alert('Formulario enviado con éxito.');
+    });
+
+    showStep(currentStep);
